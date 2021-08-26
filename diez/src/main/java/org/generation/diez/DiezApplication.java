40a7fe82f937db511c7ea8deb22362fbffc48660
Victor Martinez <victor.martinez.zamora98@gmail.com>
2021-08-25 19:44:08 -0500

    add | carpeta para IdeaProjects

---
+++ diez/src/main/java/org/generation/diez/DiezApplication.java
@@ -0,0 +1,14 @@
+package org.generation.diez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiezApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DiezApplication.class, args);
+	}//main
+
+}//class DiezAplication
